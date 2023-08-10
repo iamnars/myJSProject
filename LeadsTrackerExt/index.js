f77e2380f5ecdbb031d3d2ebe02505fb8eb8fd24
iamnars <narene.nagares@gmail.com>
2023-08-10 14:04:39 +0800

    Finished Tracker
    added localStorage and saving of active tabs to the list

--- LeadsTrackerExt/index.js
+++ LeadsTrackerExt/index.js
@@ -3,28 +3,55 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFrmLS = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
 
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
+if(leadsFrmLS){
+    myLeads = leadsFrmLS
+    render(myLeads)
+}
 
-function renderLeads(){
+function render(leads){
     let listItems = ""
-    for (let i=0; i< myLeads.length;i++){
+    for (let i=0; i< leads.length;i++){
         //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
         //const li = document.createElement("li")
        // li.textContent = myLeads[i]
        // ulEl.append(li)
        listItems += `<li>
-                        <a target="_blank" href="${myLeads[i]}"> 
-                            ${myLeads[i]} 
+                        <a target="_blank" href="${leads[i]}"> 
+                            ${leads[i]} 
                         </a>
                      </li>`
     }
     ulEl.innerHTML = listItems
 }
+
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+deleteBtn.addEventListener("dblclick", function(){
+    myLeads = []
+    localStorage.clear()
+    render(myLeads)
+})
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)   
+    })
+   
+
+})
+ 
 
 
 
