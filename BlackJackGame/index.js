2e9bb023bff2da468882b754ce80074d3ab375e4
iamnars <narene.nagares@gmail.com>
2023-08-10 11:06:20 +0800

    Finished BJ Game

--- BlackJackGame/index.js
+++ BlackJackGame/index.js
@@ -1,25 +1,37 @@
-let firstCard = 0
-let secondCard = 0
-let cards = [firstCard, secondCard] //ordered list
-let sum = firstCard + secondCard
+let cards = [] //ordered list
+let sum = 0
 let hasBJ = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl =  document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 //let sumEl = document.querySelector(".sum-el") //when calling a class
 let cardsEl = document.getElementById("cards-el")
 
+let player = {
+     playerName: "Per",
+     playerChips: 145
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.playerName + ": $" + player.playerChips
+
 function startGame(){
-    firstCard = getRandomCard()
-    secondCard = getRandomCard()
+    cards = []
+    sum = 0
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards.push(firstCard)
+    cards.push(secondCard)
+    isAlive = true
     renderGame()
 }
 
 function renderGame(){
     cardsEl.textContent = "Cards: "
     for(let i=0; i< cards.length; i++){
-        cardsEl.textContent += cards[i] + " ";
+        sum += cards[i]
+        cardsEl.textContent += cards[i] + " "
     }
 
     sumEl.textContent = "Sum: " + sum
@@ -36,10 +48,12 @@ function renderGame(){
 }
 
 function newCard(){
+   if(isAlive === true && hasBJ ===false){
+    sum = 0
     cards.push(getRandomCard())
-    sum += cards[cards.length -1]
     renderGame()
-    cardsEl.textContent += " " + cards[cards.length -1]
+   }
+   
 }
 
 function getRandomCard(){
