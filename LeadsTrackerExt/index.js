b8899854b4807d865d5dee5e7ece99683d32e67e
iamnars <narene.nagares@gmail.com>
2023-08-10 12:38:14 +0800

    CreationOfLeadsTracker
    Trying to test deploy unrefined tracker

---
+++ LeadsTrackerExt/index.js
@@ -0,0 +1,30 @@
+
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
+})
+
+function renderLeads(){
+    let listItems = ""
+    for (let i=0; i< myLeads.length;i++){
+        //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+        //const li = document.createElement("li")
+       // li.textContent = myLeads[i]
+       // ulEl.append(li)
+       listItems += `<li>
+                        <a target="_blank" href="${myLeads[i]}"> 
+                            ${myLeads[i]} 
+                        </a>
+                     </li>`
+    }
+    ulEl.innerHTML = listItems
+}
+
+
+
